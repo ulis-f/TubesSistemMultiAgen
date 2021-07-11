298f733faa49daf8185011eb48caa164074239d2
Yulius Famas <yuliusfamas@gmail.com>
2021-07-11 20:44:53 +0700

    add file

---
+++ TubesSMA/src/bookTrading/StockManager/StockManagerGui.java
@@ -0,0 +1,9 @@
+package bookTrading.StockManager;
+
+public interface StockManagerGui {
+    void setAgent(StockManagerAgent a);
+    void show();
+    void hide();
+    void notifyUser(String message);
+    void dispose();
+}   
