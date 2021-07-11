5ce52e1eadb18129a587a99c940a3019e20e43f8
Yulius Famas <yuliusfamas@gmail.com>
2021-07-11 17:58:35 +0700

    update

--- TubesSMA/src/bookTrading/seller/BookSellerAgent.java
+++ TubesSMA/src/bookTrading/seller/BookSellerAgent.java
@@ -10,7 +10,7 @@ import jade.core.behaviours.*;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 import jade.domain.*;
-import jade.domain.FIPAAgentManagement.*;
+import jade.domain.FIPAAgentManagement.*;  
 
 import java.util.*;
 
@@ -74,58 +74,6 @@ public class BookSellerAgent extends Agent {
         }
     }
 
-    /**
-     * This method is called by the GUI when the user inserts a new
-     * book for sale
-     * @param title The title of the book for sale
-     * @param initialPrice The initial price
-     * @param minPrice The minimum price
-     * @param deadline The deadline by which to sell the book
-    **/
-    public void putForSale(String title, int initPrice, int minPrice, Date deadline) {
-        addBehaviour(new PriceManager(this, title, initPrice, minPrice, deadline));
-    }
-    private class PriceManager extends TickerBehaviour {
-        private String title;
-        private int minPrice, currentPrice, initPrice, deltaP;
-        private long initTime, deadline, deltaT;
-
-        private PriceManager(Agent a, String t, int ip, int mp, Date d) {
-            super(a, 60000); // tick every minute
-            title = t;
-            initPrice = ip;
-            currentPrice = initPrice;
-            deltaP = initPrice - mp;
-            deadline = d.getTime();
-            initTime = System.currentTimeMillis();
-            deltaT = ((deadline - initTime) > 0 ? (deadline - initTime) : 60000);
-        }
-
-        public void onStart() {
-            // Insert the book in the catalogue of books available for sale
-            catalogue.put(title, this);
-            super.onStart();
-        }
-
-        public void onTick() {
-            long currentTime = System.currentTimeMillis();
-            if (currentTime > deadline) {
-                // Deadline expired
-                myGui.notifyUser("Cannot sell book "+title);
-                catalogue.remove(title);
-                stop();
-            }
-            else {
-                // Compute the current price
-                long elapsedTime = currentTime - initTime;
-                currentPrice = (int)Math.round(initPrice - 1.0 * deltaP * (1.0 * elapsedTime / deltaT));
-            }
-        }
-
-        public int getCurrentPrice() {
-            return currentPrice;
-        }
-    }
 
     private class CallForOfferServer extends CyclicBehaviour {
         private MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
@@ -137,20 +85,13 @@ public class BookSellerAgent extends Agent {
                 String title = msg.getContent();
                 myGui.notifyUser("Received Proposal to buy "+title);
                 ACLMessage reply = msg.createReply();
-                PriceManager pm = (PriceManager) catalogue.get(title);
-                if (pm != null) {
-                    // The requested book is available for sale. Reply with the price
-                    reply.setPerformative(ACLMessage.PROPOSE);
-                    reply.setContent(String.valueOf(pm.getCurrentPrice()));
-                }
-                else {
-                    // The requested book is NOT available for sale.
-                    reply.setPerformative(ACLMessage.REFUSE);
-                }
+          
+                reply.setPerformative(ACLMessage.PROPOSE);
+                    
                 myAgent.send(reply);
-                myGui.notifyUser(pm != null ? "Sent Proposal to sell at "+reply.getContent() : "Refused Proposal as the book is not for sale");
+                myGui.notifyUser("Sent Proposal to sell at "+reply.getContent());
             }
-            else{
+            else{      
                 block();
             }
         }
