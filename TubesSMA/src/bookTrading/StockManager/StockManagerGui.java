34d7f91dcc2d0e83fda6a3a720cb6f1f43cfbe2b
Yulius Famas <yuliusfamas@gmail.com>
2021-07-11 20:43:38 +0700

    delete

--- TubesSMA/src/bookTrading/StockManager/StockManagerGui.java
+++
@@ -1,9 +0,0 @@
-package bookTrading.StockManager;
-
-public interface StockManagerGui {
-    void setAgent(StockManagerAgent a);
-    void show();
-    void hide();
-    void notifyUser(String message);
-    void dispose();
-}   
